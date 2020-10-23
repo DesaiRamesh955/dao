4108a7cee16ddfcc5fd4cf8fd35e954291e84fb2
dhirendra108 <64583170+dhirendra108@users.noreply.github.com>
2020-10-23 11:48:36 +0530

    first commit

---
+++ DRDAO.php
@@ -0,0 +1,139 @@
+<?php
+
+class DRDAO{
+
+    private $con;
+        function __construct() 
+        {
+            include_once '../include/db.php';
+        
+            $db = new DB();
+            $this->con = $db->connect();
+            
+        }
+        
+        // function for insert data
+        public function insert($table,$value){
+            $field="";
+            $val="";
+            $para=[];
+            $i=0;
+            
+            foreach ($value as $k => $v) {
+                if($i == 0){
+                    $field.=$k;
+                    $val.="'".$v."'";
+                    array_push($para,$v);
+                }else {
+                    $field.=",".$k;
+                    $val.=",'".$v."'";
+                    array_push($para,$v);
+                }
+                $i++;
+            }
+          
+        
+            $parameter = "";
+
+            $num = count($value);
+            for($i=0;$i<$num;$i++){
+                if($i==$num-1){
+                    $parameter.="?";
+                }else{
+                    $parameter.="?,";
+                }
+        
+             }
+
+            
+            
+            $stmt = $this->con->prepare("insert into $table($field) values($parameter)");
+           
+        
+            if($stmt->execute($para)){
+                $id = $this->con->lastInsertId();//get insert id
+                return ['status'=>'SUCCESS',"lastID => $id"];//JSON response when success
+            }else{
+                return ["status"=>"FAILED"];
+            }
+        }
+
+        // function for select data
+        function select($table, $where='', $other='')
+        {
+            if($where != '')
+            {
+                $where = 'where ' .$where;
+            }
+              
+            
+            $stmt = $this->con->prepare("SELECT * FROM $table $where $other");
+           
+            
+            
+            if($stmt->execute()){
+                while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+                    //array_push($data,$row);
+                    $data[] = $row;
+                }
+                return $data;
+            }else{
+                echo "no_data_found";
+            }
+           }
+             // function for select data
+        function select_manual($query)
+        {
+           
+            $stmt = $this->con->prepare("$query");
+           
+            
+            
+            if($stmt->execute()){
+                while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+                    //array_push($data,$row);
+                    $data[] = $row;
+                }
+                return $data;
+            }else{
+                echo "no_data_found";
+            }
+           }
+
+            // function for select data
+        function countRow($table,$column='', $where='', $other='')
+        {
+            if($where != '')
+            {
+                $where = 'where ' .$where;
+            }
+              
+            if($column==''){
+                $column = '*';
+            }
+            $stmt = $this->con->prepare("SELECT count($column) as c FROM $table $where $other");
+            $stmt->execute();
+
+              $row =  $stmt->fetch(PDO::FETCH_ASSOC);
+              return $row['c'];
+        }
+
+        //function for delete data
+
+        function delete($table,$where=''){
+            if($where != '')
+            {
+                $where = 'where ' .$where;
+            }
+            $stmt = $this->con->prepare("DELETE FROM $table $where");
+            
+            if($stmt->execute()){
+                $id = $this->con->lastInsertId();//get insert id
+                return ['status'=>'SUCCESS'];//JSON response when success
+            }else{
+                return ["status"=>"FAILED"];
+            }
+
+        }
+}
+?>
